bfab61c494b757f4afc3a22cb132571cedb220ed
Anamol Soman <anamolsoman2@gmail.com>
2025-02-22 11:50:41 +0530

    Minor changes.

--- call-polyfill.js
+++ call-polyfill.js
@@ -1,1 +1,15 @@
 // Call Method Poly fill
+
+//  Call method is used to change the "This " of any function
+let obj = { fname: "Anamol", lname: "Soman" };
+
+function printName() {
+  console.log(this)
+  return this.fname + this.lname;
+}
+
+
+
+
+
+console.log(printName.call(obj));
