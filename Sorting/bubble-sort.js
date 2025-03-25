2769a825e91c7722eddb207284e4c5c29d302a70
Anamol Soman <anamolsoman2@gmail.com>
2025-03-25 10:11:13 +0530

    Optimized Bubble Sort.

--- Sorting/bubble-sort.js
+++ Sorting/bubble-sort.js
@@ -2,16 +2,23 @@ let arr = [5, 3, 1, 2, 4, 9, 7];
 
 function BubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
     for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp;
         temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        swapped = true;
       }
+    }
+    if (!swapped) {
+      break;
     }
   }
 }
 
+console.time("BubbleSortWithoutFlag");
 BubbleSort(arr);
+console.timeEnd("BubbleSortWithoutFlag");
 console.log(arr);
